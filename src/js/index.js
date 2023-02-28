18843573b5ef339c474f0b401ad74200fd0021eb
Szymon Michalak <sz.p.michalak@gmail.com>
2023-02-28 01:05:31 +0100

    Add flower cursor

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
 import { preloadImages } from './utils';
 import { Grid } from './grid';
 
+function removeFadeOut( el, speed ) {
+  var seconds = speed/1000;
+  el.style.transition = "opacity "+seconds+"s ease";
+
+  el.style.opacity = 0;
+  setTimeout(function() {
+      el.parentNode.removeChild(el);
+  }, speed);
+}
 
 // Preload images and initialize the grid after a 2 second delay
 setTimeout(() => {
   preloadImages('.column__item-img').then(() => {
     // Hide the loader
-    document.body.classList.remove('loading');
+    document.getElementById("loader").classList.remove('loading');
+    removeFadeOut(document.getElementById('loader'), 2000);
 
     // Initialize the grid
     new Grid(document.querySelector('.columns'));
